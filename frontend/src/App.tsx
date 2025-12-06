814448cedfd27efd669eb1558416f0cc7105c5dd
gnaiG19804 <tvgiang19082004@gmail.com>
2025-12-06 21:39:32 +0700

    update CI CD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<HomePage/>} />
            <Route path="/login" element={<LoginPage/>} />
-            <Route path="/summaries/:id" element={<HistoryPage/>} />
+            <Route path="/history/:id" element={<HistoryPage />} />
         </Routes>
       </BrowserRouter>
   );
